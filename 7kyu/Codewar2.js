e8d9a5b19a3e9792473e271d7299ed6ed9483016
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-05-30 00:15:55 +0200

    added the question within my solution

--- 7kyu/Codewar2.js
+++ 7kyu/Codewar2.js
@@ -1,3 +1,13 @@
+/*
+Write a function which takes a number as input and returns the sum of the absolute 
+value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+    10 --> 1
+    99 --> 18
+    -32 --> 5
+*/
+
 function sumDigits(number) {
   number = Math.abs(number);
   number = Array.from(String(number),Number);
