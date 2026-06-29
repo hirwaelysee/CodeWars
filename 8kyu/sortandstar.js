d35deb1555f7b6c7e38d62b8e8d9af59f6bcb7ad
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-06-29 22:44:29 +0200

    feat: 8kyu level kata

---
+++ 8kyu/sortandstar.js
@@ -0,0 +1,14 @@
+/*
+You will be given a list of strings. You must sort it alphabetically 
+(case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+function twoSort(s) {
+  //1. sort the list of strings.
+  //2. split the strings.
+  //3. add *** between each letter.
+  return s.sort()[0].split("").join("***");
+
+}
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
