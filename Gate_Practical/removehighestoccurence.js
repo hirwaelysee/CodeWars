211e0b5a382c1f31e132ee51a497188de35d152f
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-07-04 00:30:19 +0200

    Question asked in js fundamentals in gate 1

---
+++ Gate_Practical/removehighestoccurence.js
@@ -0,0 +1,33 @@
+/*
+Write a function removeHighestOccurrence that takes an array of elements as an argument and 
+returns a new array with the highest occurring element removed. 
+
+removeHighestOccurrence([1, 2, 2, 3, 4, 4, 5]) // [1, 3, 5]
+
+removeHighestOccurrence(['a', 'b', 'b', 'c', 'c', 'c', 'd']) // ['a', 'b', 'b', 'd']
+
+removeHighestOccurrence([true, true, false, false, true]) // [false, false]
+*/
+function removeHighestOccurrence(arr){
+    const obj = {}
+    for(let i=0; i<arr.length; i++){
+        let count =0;
+        for(let j=0; j<arr.length; j++){
+            if(arr[i] === arr[j]){
+                count++
+            }
+        }
+        obj[arr[i]] = count;
+    }
+    
+    let highestValue= -Infinity;
+    for (const key in obj){
+        if(obj[key] > highestValue){
+            highestValue = obj[key];
+        }
+    }
+    return arr.filter((item)=> item !== highestValue);
+
+}
+console.log(removeHighestOccurrence([1, 2, 2, 3, 4, 4, 5]))
+//Not yet finished still finding it out.
