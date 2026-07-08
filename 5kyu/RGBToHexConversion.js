10679577236c8ccdfefeaea87177ba9b50d38a5a
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-07-08 10:08:40 +0200

    feat: add kata RGBToHexConversion which converts numbers to base 16

---
+++ 5kyu/RGBToHexConversion.js
@@ -0,0 +1,21 @@
+/*
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+Examples (input --> output):
+255, 255, 255 --> "FFFFFF"
+255, 255, 300 --> "FFFFFF"
+0, 0, 0       --> "000000"
+148, 0, 211   --> "9400D3"
+*/
+function rgb(r, g, b) {
+    r = Math.min(255,Math.max(0,r));
+    g = Math.min(255,Math.max(0,g));
+    b = Math.min(255,Math.max(0,b));
+    
+    r = r.toString(16).toUpperCase();
+    g= g.toString(16).toUpperCase();
+    b= b.toString(16).toUpperCase();
+    return `${String(r).padStart('2',0)}${String(g).padStart('2',0)}${String(b).padStart('2',0)}`;
+}
