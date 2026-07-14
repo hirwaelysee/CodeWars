67577ffd305b9a36939aa4245b724553a2225614
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-07-14 22:24:33 +0200

    add a switchitup kata

---
+++ 8kyu/switchitup.js
@@ -0,0 +1,21 @@
+/*
+When provided with a number between 0-9, return it in words. Note that the input is 
+guaranteed to be within the range of 0-9.
+
+Input: 1
+
+Output: "One".
+*/
+function switchItUp(number){
+//Write your own Code!
+  if(number === 0) return 'Zero'
+  if(number === 1) return 'One'
+  if(number === 2) return 'Two'
+  if(number === 3) return 'Three'
+  if(number === 4) return 'Four'
+  if(number === 5) return 'Five'
+  if(number === 6) return 'Six'
+  if(number === 7) return 'Seven'
+  if(number === 8) return 'Eight'
+  if(number === 9) return 'Nine'
+}
