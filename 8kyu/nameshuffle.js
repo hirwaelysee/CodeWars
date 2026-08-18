075c547a5c42affb0ce5415ab9769a99bb63f131
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-08-18 14:15:13 +0200

    Add a name shuffle kata

---
+++ 8kyu/nameshuffle.js
@@ -0,0 +1,15 @@
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+*/
+
+function nameShuffle(str){
+    return str
+           .split(' ')
+           .reverse()
+           .join(' ')
+}
+console.log(nameShuffle("john McClane" ));
