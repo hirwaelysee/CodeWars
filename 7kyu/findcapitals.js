39a0528b416654adaf027f069abbc0842a830396
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-08-19 12:02:42 +0200

    Add a findcapitals kata

---
+++ 7kyu/findcapitals.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters
+(word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) 
+letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]
+*/
+
+var capitals = function (word) {
+  return word.split('').map((item,index)=>{
+      if(item == item.toUpperCase()){
+        return index
+      }
+  }).filter(item => item != undefined)
+};
+console.log(capitals("CodEWaRs"));
