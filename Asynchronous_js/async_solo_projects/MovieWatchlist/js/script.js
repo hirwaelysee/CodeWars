dc975566c0edb4e1c220b5c9ea04e21342d0fb50
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-08-23 21:00:08 +0200

    Add changes in the script file

--- Asynchronous_js/async_solo_projects/MovieWatchlist/js/script.js
+++ Asynchronous_js/async_solo_projects/MovieWatchlist/js/script.js
@@ -1,4 +1,3 @@
-
 // api to use https://www.omdbapi.com/?apikey=89be20f3
 // link to the documentation: https://www.omdbapi.com/
 
@@ -6,9 +5,8 @@ const btn = document.getElementById('btn');
 const main1 = document.getElementById('main1');
 const content = document.getElementById('content');
 const explore = document.getElementById('exploring');
-const watch = document.getElementById('watch');
 
-const mainSection = document.getElementById("mainsection")
+
 /* 
 Requirements:
 1. Two pages: index.html, and watchlist.html
@@ -134,57 +132,3 @@ function addLocalStorage(id, title, year, poster){
 
    alert(`${title} has been added to your watchlist!`)
 }
-
-function watchList(){
-   let fromLocal = JSON.parse(localStorage.getItem('myWatchlist')) || [];
-
-   if(fromLocal.length > 0){
-      watch.style.display = "none";
-
-      let display =``
-      for(let item of fromLocal){
-         display = `
-                     <div class=" w-[45%]  flex justify-between gap-x-4 p-2">
-
-                     <div class=" w-1/5">
-                        <img 
-                           src="${item.Poster}"
-                           alt="movie image"
-                           class="h-full w-full"
-                           >
-                     </div>
-
-                     <div class=" w-4/5 flex flex-col gap-y-1">
-
-                        <div class="flex gap-x-2 items-center">
-                           <h4 class="font-inter font-semibold text-2xl">${item.Title} ${item.Year}</h4>
-                           <p class="text-sm font-inter">⭐ 8.1</p>
-                        </div>
-
-                        <div class="flex gap-x-6 text-base">
-                           <p>117 min</p>
-                           <p>Action, Drama, Sci-fi</p>
-                           <div class="flex justify-between  gap-x-1 items-center cursor-pointer" onclick="addLocalStorage('${item.imdbID}','${item.Title.replace(/'/g, "\\'")}','${item.Year}','${item.Poster}')">
-                                 <img 
-                                    src="./assets/subtractor.png" 
-                                    alt=""
-                                    class="w-4 h-4"
-                                    >
-                                 <p class="text-sm text-black">Watchlist</p>
-                           </div>
-                        </div>
-
-                        <p class="text-lg text-gray-500">A blade runner must pursue and terminate four <br> replicants who stole a ship in space, and have <br> returned to Earth to find their creator.</p>
-                     
-                     </div>
-
-               </div>
-               
-               <hr class="my-6 border-1 border-[#E5E7EB] w-[44%] ">   
-                  
-               `
-      }
-      mainSection.innerHTML = display;
-   }
-}
-watchList()
